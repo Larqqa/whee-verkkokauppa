56b422ab48032f9aa53a15a4f8efa4c8a18ffc7f
Larqqa <32098438+Larqqa@users.noreply.github.com>
2021-04-01 23:22:13 +0300

    Feat: front page design finished
    Main design and responsive design done.

--- src/components/header/Cart.js
+++ src/components/header/Cart.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import cart from '../../assets/icons/shopping_cart_black_24dp.svg';
+import cart from '../../assets/icons/shopping_cart_white_24dp.svg';
 import './Cart.scss';
 
 const Cart = (props) => {
   return (
     <div className="shopping-cart">
-      <Link to="/ostoskori">
-        <img src={cart} className="cart" alt="cart" />
+      <p className="cart-info">No items in cart</p>
+      <Link to="/ostoskori" className="cart-icon">
+        <img src={cart} alt="cart" />
       </Link>
     </div>
   );
